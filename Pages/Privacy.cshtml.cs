98d2c429e4870e98c68219bf9b4aaee812210b3b
saubhik.banik <saubhik.banik@outlook.com>
2025-03-07 03:39:54 +0530

    Chnages for Appllication Insight

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -14,6 +14,7 @@ namespace AzureTestDeploy.Pages
 
         public void OnGet()
         {
+            throw new Exception("This is a test exception");
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AzureTestDeploy
 
             // Add services to the container.
             builder.Services.AddRazorPages();
-
+            builder.Services.AddApplicationInsightsTelemetry();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
